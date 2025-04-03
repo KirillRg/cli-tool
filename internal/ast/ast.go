94407da1cd2640a096197f4cd87f363a21f969db
Кирилл <r.k.a.77@mail.ru>
2025-04-04 01:28:33 +0300

    [CLI Development] Added to AST generation logic headers, body and params

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,68 +1,40 @@
 package ast
 
-// Kind определяет тип узла
-type Kind string
-
-const (
-	ProgramKind             Kind = "Program"
-	ExpressionStatementKind Kind = "ExpressionStatement"
-	CallExpressionKind      Kind = "CallExpression"
-	LiteralKind             Kind = "Literal"
-	IdentifierKind          Kind = "Identifier"
-)
-
-type Node interface {
-	GetKind() Kind
-}
-
-// ======== Интерфейсы ========
-
-type StatementNode interface {
-	Node
-	statementNode()
-}
-type ExpressionNode interface {
-	Node
-	expressionNode()
-}
-
-// ======== Узлы ========
+// Структуры AST по стандарту ESTree
 
 type Program struct {
-	Body []StatementNode
+	Body []Node
 }
 
-func (p Program) GetKind() Kind { return ProgramKind }
-
-// Линкуем Expression вместе с Statement`ом
 type ExpressionStatement struct {
-	Expression ExpressionNode
+	Expression CallExpression
 }
 
-func (ExpressionStatement) GetKind() Kind  { return ExpressionStatementKind }
-func (ExpressionStatement) statementNode() {}
-
-// Как правило вызов функции
 type CallExpression struct {
-	Callee    ExpressionNode
-	Arguments []ExpressionNode
+	Callee    MemberExpression
+	Arguments []Node
 }
 
-func (CallExpression) GetKind() Kind   { return CallExpressionKind }
-func (CallExpression) expressionNode() {}
+type MemberExpression struct {
+	Object   Identifier
+	Property Identifier
+}
 
-//Имя переменной
 type Identifier struct {
 	Name string
 }
 
-func (Identifier) GetKind() Kind   { return IdentifierKind }
-func (Identifier) expressionNode() {}
+type ObjectExpression struct {
+	Properties []Property
+}
 
-//Ее значение
+type Property struct {
+	Key   Identifier
+	Value Node
+}
+
 type Literal struct {
 	Value string
 }
 
-func (Literal) GetKind() Kind   { return LiteralKind }
-func (Literal) expressionNode() {}
+type Node interface{}
--- internal/ast/generator.go
+++ internal/ast/generator.go
@@ -4,40 +4,71 @@ import (
 	"github.com/KirillRg/cli-tool/internal/parser"
 )
 
-func GenerateAST(collection *parser.InsomniaCollection) *Program {
-	var statements []StatementNode
-
+// Генерация AST для коллекции
+func GenerateAST(collection *parser.InsomniaCollection) Program {
+	var body []Node
 	for _, req := range collection.Collection {
-		method := req.Method
-		url := req.URL
-
-		call := CallExpression{
-			Callee: Identifier{Name: getHttpFunctionName(method)},
-			Arguments: []ExpressionNode{
-				Literal{Value: url},
-			},
-		}
-
-		stmt := ExpressionStatement{Expression: call}
-		statements = append(statements, stmt)
+		body = append(body, GenerateRequestAST(req))
 	}
+	return Program{Body: body}
+}
 
-	return &Program{
-		Body: statements,
+// Генерация универсального запроса в AST
+func GenerateRequestAST(req parser.RequestItem) Node {
+	return ExpressionStatement{
+		Expression: CallExpression{
+			Callee: MemberExpression{
+				Object:   Identifier{Name: "http"},
+				Property: Identifier{Name: req.Method},
+			},
+			Arguments: []Node{
+				Literal{Value: req.URL},
+				ObjectExpression{
+					Properties: []Property{
+						{Key: Identifier{Name: "headers"}, Value: GenerateHeaders(req.Headers)},
+						{Key: Identifier{Name: "params"}, Value: GenerateParameters(req.Parameters)},
+						{Key: Identifier{Name: "body"}, Value: GenerateBody(req.Body)},
+					},
+				},
+			},
+		},
 	}
 }
 
-func getHttpFunctionName(method string) string {
-	switch method {
-	case "GET":
-		return "http.get"
-	case "POST":
-		return "http.post"
-	case "PUT":
-		return "http.put"
-	case "DELETE":
-		return "http.del"
-	default:
-		return "http.unknown"
+// Генерация параметров запроса
+func GenerateParameters(params []parser.RequestParam) Node {
+	properties := []Property{}
+	for _, param := range params {
+		if !param.Disabled {
+			properties = append(properties, Property{
+				Key:   Identifier{Name: param.Name},
+				Value: Literal{Value: param.Value},
+			})
+		}
+	}
+	return ObjectExpression{Properties: properties}
+}
+
+// Генерация заголовков запроса
+func GenerateHeaders(headers []parser.RequestHeader) Node {
+	properties := []Property{}
+	for _, header := range headers {
+		if !header.Disabled {
+			properties = append(properties, Property{
+				Key:   Identifier{Name: header.Name},
+				Value: Literal{Value: header.Value},
+			})
+		}
+	}
+	return ObjectExpression{Properties: properties}
+}
+
+// Генерация тела запроса
+func GenerateBody(body parser.RequestBody) Node {
+	return ObjectExpression{
+		Properties: []Property{
+			{Key: Identifier{Name: "mimeType"}, Value: Literal{Value: body.MimeType}},
+			{Key: Identifier{Name: "text"}, Value: Literal{Value: body.Text}},
+		},
 	}
 }
