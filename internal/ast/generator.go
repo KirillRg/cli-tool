79cbf0a5ea75900cd87c90fa8af39cf0826e3d35
Кирилл <r.k.a.77@mail.ru>
2025-04-04 23:38:45 +0300

    [CLI Development] Added to AST generation logic of dealing with null or disabled headers, body and params

--- internal/ast/generator.go
+++ internal/ast/generator.go
@@ -1,8 +1,6 @@
 package ast
 
-import (
-	"github.com/KirillRg/cli-tool/internal/parser"
-)
+import "github.com/KirillRg/cli-tool/internal/parser"
 
 // Генерация AST для коллекции
 func GenerateAST(collection *parser.InsomniaCollection) Program {
@@ -15,42 +13,61 @@ func GenerateAST(collection *parser.InsomniaCollection) Program {
 
 // Генерация универсального запроса в AST
 func GenerateRequestAST(req parser.RequestItem) Node {
+	// Универсальная генерация для всех типов запросов
+	args := []Node{Literal{Value: req.URL}}
+
+	properties := []Property{}
+
+	// Генерация заголовков, если они есть и активны
+	if len(req.Headers) > 0 {
+		headers := GenerateHeaders(req.Headers)
+		if len(headers.Properties) > 0 {
+			properties = append(properties, Property{
+				Key:   Identifier{Name: "headers"},
+				Value: headers,
+			})
+		}
+	}
+
+	// Генерация параметров, если они есть и активны
+	if len(req.Parameters) > 0 {
+		params := GenerateParameters(req.Parameters)
+		if len(params.Properties) > 0 {
+			properties = append(properties, Property{
+				Key:   Identifier{Name: "params"},
+				Value: params,
+			})
+		}
+	}
+
+	// Генерация тела запроса, если оно есть
+	if req.Body.MimeType != "" || req.Body.Text != "" {
+		properties = append(properties, Property{
+			Key:   Identifier{Name: "body"},
+			Value: GenerateBody(req.Body),
+		})
+	}
+
+	// Добавляем свойства (headers, params, body) в аргументы
+	if len(properties) > 0 {
+		args = append(args, ObjectExpression{Properties: properties})
+	}
+
+	// Формируем окончательную структуру вызова
 	return ExpressionStatement{
 		Expression: CallExpression{
+
 			Callee: MemberExpression{
 				Object:   Identifier{Name: "http"},
 				Property: Identifier{Name: req.Method},
 			},
-			Arguments: []Node{
-				Literal{Value: req.URL},
-				ObjectExpression{
-					Properties: []Property{
-						{Key: Identifier{Name: "headers"}, Value: GenerateHeaders(req.Headers)},
-						{Key: Identifier{Name: "params"}, Value: GenerateParameters(req.Parameters)},
-						{Key: Identifier{Name: "body"}, Value: GenerateBody(req.Body)},
-					},
-				},
-			},
+			Arguments: args,
 		},
 	}
 }
 
-// Генерация параметров запроса
-func GenerateParameters(params []parser.RequestParam) Node {
-	properties := []Property{}
-	for _, param := range params {
-		if !param.Disabled {
-			properties = append(properties, Property{
-				Key:   Identifier{Name: param.Name},
-				Value: Literal{Value: param.Value},
-			})
-		}
-	}
-	return ObjectExpression{Properties: properties}
-}
-
 // Генерация заголовков запроса
-func GenerateHeaders(headers []parser.RequestHeader) Node {
+func GenerateHeaders(headers []parser.RequestHeader) ObjectExpression {
 	properties := []Property{}
 	for _, header := range headers {
 		if !header.Disabled {
@@ -63,12 +80,34 @@ func GenerateHeaders(headers []parser.RequestHeader) Node {
 	return ObjectExpression{Properties: properties}
 }
 
-// Генерация тела запроса
-func GenerateBody(body parser.RequestBody) Node {
-	return ObjectExpression{
-		Properties: []Property{
-			{Key: Identifier{Name: "mimeType"}, Value: Literal{Value: body.MimeType}},
-			{Key: Identifier{Name: "text"}, Value: Literal{Value: body.Text}},
-		},
+// Генерация параметров запроса
+func GenerateParameters(params []parser.RequestParam) ObjectExpression {
+	properties := []Property{}
+	for _, param := range params {
+		if !param.Disabled {
+			properties = append(properties, Property{
+				Key:   Identifier{Name: param.Name},
+				Value: Literal{Value: param.Value},
+			})
+		}
 	}
+	return ObjectExpression{Properties: properties}
+}
+
+// Генерация тела запроса
+func GenerateBody(body parser.RequestBody) ObjectExpression {
+	properties := []Property{}
+	if body.MimeType != "" {
+		properties = append(properties, Property{
+			Key:   Identifier{Name: "mimeType"},
+			Value: Literal{Value: body.MimeType},
+		})
+	}
+	if body.Text != "" {
+		properties = append(properties, Property{
+			Key:   Identifier{Name: "text"},
+			Value: Literal{Value: body.Text},
+		})
+	}
+	return ObjectExpression{Properties: properties}
 }
